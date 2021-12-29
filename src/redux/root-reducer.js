1cbabb1bca4dd464e55c4d7399468a10a0e5ad9e
CAndresHerediaA <a.heredia@expandya.com>
2021-12-29 09:49:42 -0500

    ADD REDUX PERSIST

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,18 @@
-import {combineReducers } from 'redux'
+import { combineReducers } from "redux";
+import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //Emulate localStorage
 
-import userReducer from './user/user.reducer'
-import cartReducer from './cart/cart.reducer'
+const persistConfg = {
+     key:'root',
+     storage,
+     whitelist:['cart']
+};
 
-export default combineReducers ({
-     user:userReducer,
-     cart:cartReducer
-})
+const rootReducer= combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+export default persistReducer(persistConfg, rootReducer);
